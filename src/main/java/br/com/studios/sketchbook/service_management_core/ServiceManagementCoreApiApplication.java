cd62749d23fc0deb120d70d75479302b4aec14ec
Thiago Barbosa <curiosidades381@gmail.com>
2025-07-21 19:13:18 -0300

    ADD
    *ProductModel criado
    (Criação do primeiro modelo, com id e outros valores)

--- src/main/java/br/com/studios/sketchbook/service_management_core/ServiceManagementCoreApiApplication.java
+++ src/main/java/br/com/studios/sketchbook/service_management_core/ServiceManagementCoreApiApplication.java
@@ -1,13 +1,15 @@
 package br.com.studios.sketchbook.service_management_core;
 
+import br.com.studios.sketchbook.service_management_core.models.ProductModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServiceManagementCoreApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceManagementCoreApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceManagementCoreApiApplication.class, args);
+
+    }
 
 }
---
+++ src/main/java/br/com/studios/sketchbook/service_management_core/models/ProductModel.java
@@ -0,0 +1,63 @@
+package br.com.studios.sketchbook.service_management_core.models;
+
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PRODUCTS")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+public class ProductModel implements Serializable {
+    @Serial
+    /// Número de série da entidade
+    private static final long serialVersionUID = 1L;
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false)
+    @EqualsAndHashCode.Include
+    @Getter
+    @Id
+    /// Id geral do produto
+    private UUID id;
+
+    @Getter
+    /// Nome do produto
+    private String name;
+
+    @Getter
+    /// Valor do produto
+    private BigDecimal value;
+
+    @Getter
+    /// Código de barras do produto
+    private String barcode;
+
+    public ProductModel(String name, double value, String barcode) {
+        this.name = name;
+        this.value = BigDecimal.valueOf(value);
+        this.barcode = barcode;
+    }
+
+    public void setValue(double value) {
+        this.value = BigDecimal.valueOf(value);
+    }
+
+    @Override
+    public String toString() {
+        return "ProductModel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", value=" + value +
+                ", barcode='" + barcode + '\'' +
+                '}';
+    }
+}

614a8e9436cca8fbb035c707757a8868d58fd4be
Thiago Barbosa <curiosidades381@gmail.com>
2025-07-21 16:49:56 -0300

    ProjectSetup

---
+++ src/test/java/br/com/studios/sketchbook/service_management_core/ServiceManagementCoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.studios.sketchbook.service_management_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceManagementCoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
